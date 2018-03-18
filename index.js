9d69ca1f79e350fbef9f7dbc74ece0b7fd49d330
ramishalah <romioo1010@gmail.com>
2018-03-18 19:29:04 +0300

    adding the database credintials

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
@@ -8,9 +7,9 @@ const mysql = require('mysql');
 var con = mysql.createPool({
   connectionLimit : 10,
   host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "b5654c20bfc08d",
-  password: "494c90c0",
-  database: "heroku_1c981effd06b6ae"
+  user: "bd5e1294bfda78",
+  password: "1ab708b1",
+  database: "heroku_24f121bc0d498a9"
 });
 
 
